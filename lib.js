63482b96053025a0777c90376c0fef2050221d22
lllillly <qkdgywls0503@naver.com>
2024-04-08 15:09:12 +0900

    6week

---
+++ lib.js
@@ -0,0 +1,8 @@
+function over(obj) {
+  obj.src =
+    "/Users/user/Documents/대학/3-1/웹프로그래밍/실습/트럼프 앞면.png";
+}
+function out(obj) {
+  obj.src =
+    "/Users/user/Documents/대학/3-1/웹프로그래밍/실습/트럼프 뒷면.png";
+}
